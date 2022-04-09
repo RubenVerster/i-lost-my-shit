80109b0ad5079cb1d4dca2a1446db19f59ca63cd
verster <rrverster31415@gmail.com>
2022-04-09 04:14:24 +0200

    total styling

--- components/Info.tsx
+++ components/Info.tsx
@@ -5,7 +5,7 @@ const Info = () => {
   const localdata = JSON.parse(JSON.stringify(data));
   return (
     <div className="info">
-      <div>
+      <div className="info_total">
         <CountUp end={localdata.stats.total} duration={1.11} />
       </div>
     </div>
