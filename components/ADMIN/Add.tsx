747d58f2adfac478ca1045e1414cf2b4bb7988f1
verster <rrverster31415@gmail.com>
2022-04-10 22:15:05 +0200

    add functionailty and uuid

--- components/ADMIN/Add.tsx
+++ components/ADMIN/Add.tsx
@@ -1,25 +1,44 @@
+import { addDoc, collection } from "firebase/firestore";
 import { FormEvent, useState } from "react";
+import db from "../../firebase";
 import { Level } from "../../types";
+import { v4 as uuidv4 } from "uuid";
 
 const Add = () => {
   const [title, setTitle] = useState("");
   const [option, setOption] = useState<Level | null>(null);
   const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     setLoading(true);
 
     if (title.length < 7 || !option) {
+      setErrorMsg("You missing something man?");
       setError(true);
       return;
     }
 
-    console.log(title, option);
+    try {
+      const docRef = await addDoc(collection(db, "instances"), {
+        name: title,
+        option: option,
+        date: new Date(),
+        id: uuidv4(),
+      });
+    } catch (error) {
+      setError(true);
+      setErrorMsg("Failed saving");
+      return;
+    }
+
     setOption(null);
     setTitle("");
+    setSuccess(true);
     setLoading(false);
   };
 
@@ -31,12 +50,14 @@ const Add = () => {
         }}
         className="add_form"
       >
-        {error && <p className="error">You missing something man?</p>}
+        {error && <p className="error">{errorMsg}</p>}
+        {success && <p className="success">Instance Added Successfully</p>}
 
         <input
           className="add_form_input"
           type="text"
           onChange={(e) => {
+            setSuccess(false);
             setError(false);
             setTitle(e.target.value);
           }}
